ca456f36de032cb3364063bd7fdb9e7732a784dd
Elijah Khin <workbiscap@gmail.com>
2022-02-09 18:22:21 +0300

    new_update imac

--- bonus/main.c
+++ bonus/main.c
@@ -1,43 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbrs.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhiedi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/17 20:37:04 by fhiedi            #+#    #+#             */
-/*   Updated: 2022/02/09 18:18:18 by fhiedi           ###   ########.fr       */
+/*   Created: 2022/02/09 16:50:57 by fhiedi            #+#    #+#             */
+/*   Updated: 2022/02/09 17:47:21 by fhiedi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "header.h"
 
-int	ft_putnbr_u(unsigned int nb)
+int main(void)
 {
-	char		p;
-	static int	count;
-
-	count = 0;
-	if (nb >= 10)
-		ft_putnbr_u(nb / 10);
-	p = (nb % 10) + '0';
-	write(1, &p, 1);
-	count++;
-	return (count);
-}
-
-int	ft_putnbr(int nb)
-{
-	static int	count;
-
-	count = 0;
-	if (nb < 0)
-	{
-		write(1, "-", 1);
-		count += ft_putnbr_u(-nb);
-		count++;
-	}
-	else
-		count += ft_putnbr_u(nb);
-	return (count);
+	printf("%d\n", ft_printf("Hello %c %s", 'J', "world!"));
+	printf("%d\n", printf("Hello %c %s", 'J', "world!"));
+	return (0);
 }
