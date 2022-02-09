011c57317e47328bc82f8ef309f544573fa469cf
ElijahKhin <workbiscap@gmail.com>
2022-02-09 15:00:22 +0300

    update

--- bonus/header.h
+++ bonus/header.h
@@ -6,7 +6,7 @@
 /*   By: fhiedi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 17:01:41 by fhiedi            #+#    #+#             */
-/*   Updated: 2022/02/08 17:03:03 by fhiedi           ###   ########.fr       */
+/*   Updated: 2022/02/08 20:02:46 by fhiedi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef HEADER_H
@@ -14,6 +14,13 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdio.h>
+
+typedef struct row_info
+{
+	const char *format;
+	va_list	arg_ptr;
+} t_rinfo;
 
 #endif
 
--- bonus/printf.c
+++ bonus/printf.c
@@ -6,14 +6,44 @@
 /*   By: fhiedi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:59:41 by fhiedi            #+#    #+#             */
-/*   Updated: 2022/02/08 17:10:02 by fhiedi           ###   ########.fr       */
+/*   Updated: 2022/02/09 14:59:52 by fhiedi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+int parser(char ch, va_list arg_ptr)
+{
+	void *ret;
+	int printed;
+
+	ret = va_arg(arg_ptr, void *);
+	printed = 0;
+	if (ch == 'c')
+		printed += write(1, &ret, 1);
+	return (++printed);
+}
 
 int ft_printf(const char *format, ...)
 {
-	va_list arg_ptr;
+	int		runner;
+	int		printed;
+	va_list	arg_ptr;
 
+	runner = 0;
+	printed = 0;
+	va_start(arg_ptr, format);
+	while (format[runner] != '\0')
+	{
+		if (format[runner] == '%')
+			printed += parser(format[++runner], arg_ptr);
+		else
+			printed += write(1, &format[runner++], 1);
+	}
+	return(0);
+}
+
+int main(void)
+{
+	ft_printf("Hello %c", 'J');
+	return (0);
 }
