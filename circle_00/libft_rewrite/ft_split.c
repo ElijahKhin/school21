517268d4711d0627e52145dd47066c6dfceaee7a
ElijahKhin <workbiscap@gmail.com>
2022-12-07 14:53:54 +0300

    libft_rewrite_day_06 / made short split

--- circle_00/libft_rewrite/ft_split.c
+++ circle_00/libft_rewrite/ft_split.c
@@ -5,16 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fhiedi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/01 14:21:46 by fhiedi            #+#    #+#             */
-/*   Updated: 2022/12/02 09:49:13 by fhiedi           ###   ########.fr       */
+/*   Created: 2022/12/02 09:58:02 by fhiedi            #+#    #+#             */
+/*   Updated: 2022/12/07 14:51:53 by fhiedi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
 
-static int get_number_of_rows(char const *s, unsigned char c)
+static int get_num_of_rows(char const *s, unsigned char c)
 {
     int i;
     int number_of_rows;
@@ -33,94 +31,41 @@ static int get_number_of_rows(char const *s, unsigned char c)
     return number_of_rows;
 }
 
-/*static void free_splited_row(char **splited_row, char const *s, unsigned char c)
+static void fill_array(char ***array, char const *s, unsigned char c, int num_of_rows)
 {
     int i;
-    int number_of_rows;
-
-    number_of_rows = get_number_of_rows(s, c);
-    i = 0;
-    while (i < number_of_rows)
-    {
-        if (splited_row[i])
-            free(splited_row[i]);
-        i++;
-    }
-    free(splited_row);
-}*/
-
-
-
-static void get_splited_row(char *row, char const *s, unsigned char c)
-{
-    static int i;
     int j;
-    
+    int start;
+
+    i = 0;
     j = 0;
-    while (s[i])
+    while (num_of_rows-- > 0) 
     {
-        while(s[i] != c && s[i])
-        {
-            row = malloc(1);
-            if (!row)
-                free_splited_;
-            row[j++] = s[i++]; 
-            if (s[i] == c || !s[i])
-                return ;
-        }
-    }
-
-
-
+        while (s[i] == c && s[i])
             i++;
         start = i;
         while(s[i] != c && s[i])
-        {
             i++;
-            if (s[i] == c || !s[i])
-            {
-                len = i - start;
-                splited_row[j] = ft_substr(s, start, len);
-                printf("%s\n", splited_row[j]);
-                if (!splited_row[j])
-                {
-                     while (j > 0)
-                         free(splited_row[j--]);
-                     free(splited_row);
-                     return ;
-                }
-            }
-        }
+        (*array)[j] = malloc(sizeof(char) * (i - start + 1));
+        ft_strlcpy((*array)[j], s + start, i - start + 1);
         j++;
     }
-    splited_row[j] = NULL;
+    (*array)[j] = NULL;
 }
-
 
 char **ft_split(char const *s, char c)
 {
-    int number_of_rows;
-    char **splited_row;
-
-    if (!s)
-        return 0;
-    number_of_rows = get_number_of_rows(s, c);
-    printf("number of rows: %d\n", number_of_rows + 1);
-    splited_row = malloc(sizeof(char **) * number_of_rows);
-    if (!splited_row)
+    int num_of_rows;
+    char **array;
+    
+    num_of_rows = get_num_of_rows(s, c); 
+    array = malloc(sizeof(char *) * (num_of_rows + 1));
+    if (!array)
+    {
+        free(array);
         return NULL;
-    while (number_of_rows-- > 0)
-        get_row(splited_row[i++], c, s);
-    return splited_row;
+    }
+    fill_array(&array, s, c, num_of_rows);
+    return array;
 }
 
-
-int main(void) {
-    char *splitme;
-    char **splited_row;
-
-    splitme = strdup("Tripouille ");
-    splited_row = ft_split(splitme, ' ');
-    printf("%s\n", splited_row[0]);
-    printf("%s\n", splited_row[1]);
-}
